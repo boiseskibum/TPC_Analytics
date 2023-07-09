e3e90469a4539c4fd573eaca44af9d210327e97e
boiseskibum <boiseskibum@gmail.com>
2023-07-08 18:41:22 -0600

    added athletes_testing.csv for testing the future athletes object that is yet to be built

--- share/jt_athletes.py
+++ share/jt_athletes.py
@@ -15,7 +15,26 @@ class JT_athletes:
 
     # this is typically "left" or right
     def get_injured_side(self, athlete_name):
-        injured = self.df.loc[self.df["athletes_name"] == athlete_name, "injured"].values
-        return injured_status[0]
+        filtered_df = self.df[self.df["athletes_name"] == athlete_name]
+        injured = filtered_df["injured"].values[0]
+        return injured
 
+
+#### Main ##########################################################
+
+if __name__ == "__main__":
+
+    # Test code
+    file_path = 'athletes_testing.csv'  # Replace with the actual path to your CSV file
+
+    # Create an instance of the DataProcessor
+    athletes_obj = JT_athletes(file_path)
+
+    # Get all unique types
+    athletes = athletes_obj.get_athletes()
+    print("Unique Types:", athletes)
+
+    for athlete in athletes:
+        injured = athletes_obj.get_injured_side(athlete)
+        print(f"athlete: {athlete}, Injured: {injured}")
 
